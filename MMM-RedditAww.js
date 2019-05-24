ee1b5efd261e16f8dde99faf7e9dc238de92acf3
Mike Truax <mike@miketruax.com>
2019-05-24 16:16:12 -0500

    Finalizes Aww MMM

--- MMM-RedditAww.js
+++ MMM-RedditAww.js
@@ -1,5 +1,4 @@
 /* global Module */
-
 /* Magic Mirror
  * Module: MM Reddit Posts From r/aww
  *
@@ -11,7 +10,7 @@ Module.register("MMM-RedditAww", {
 
 	// Default module config.
 	defaults: {
-		postChangeTime: 60000,
+		postChangeTime: 10000,
 		postUpdateTime: 1200000,
 		showVideos: true
 	},
@@ -31,8 +30,8 @@ Module.register("MMM-RedditAww", {
 			config.showVideos = this.defaults.showVideos;
 		}
 		this.getPosts();
-		setInterval(this.getPosts, this.config.postUpdateTime);
-
+		let self = this;
+		setInterval(function(){self.getPosts()}, this.config.postUpdateTime);
 	},
 	getStyles: function () {
 		return ["MMM-RedditAww.css"];
@@ -43,12 +42,12 @@ Module.register("MMM-RedditAww", {
 			clearInterval(this.changeInterval);
 		}
 		let self = this;
-		fetch('https://www.reddit.com/r/writingprompts/hot.json')
+		fetch('https://www.reddit.com/r/aww/hot.json')
 			.then(res => res.json())
 			.then(json => json.data.children)
 			.then(posts => posts.filter(p => {
 				if (!self.showVideos) {
-					return !p.data.sticky && !p.data.is_video
+					return !p.data.stickied && !p.data.is_video
 				}
 				return !p.data.stickied
 			}
@@ -56,37 +55,43 @@ Module.register("MMM-RedditAww", {
 			.then(posts => {
 				self.posts = posts;
 				self.updateDom();
-				self.changeInterval = setInterval(self.updateDom(), self.config.postChangeTime)
+				self.changeInterval = setInterval(
+					function(){self.updateDom()}, self.config.postChangeTime)
 			})
 	},
 	// Override dom generator.
 	getDom: function () {
-		let rdmPost = this.posts[Math.floor(Math.random()*this.posts.length)].data
 		let wrapper = document.createElement("div");
+		if(this.posts.length == 0){
+			return wrapper;
+		}
+		let rdmPost = this.posts[Math.floor(Math.random()*this.posts.length)].data
+		if(rdmPost.crosspost_parent_list){
+			rdmPost = rdmPost.crosspost_parent_list[0];
+		}
 		wrapper.classList.add("aww-container");
 		let title = document.createElement("div")
 		title.classList.add("aww-title");
 		title.innerText = rdmPost.title;
 		wrapper.appendChild(title);
-		let mediaElement = rdmPost.is_video ? this.buildVideo(rdmPost) : this.buildImg(rdmPost)
-		wrapper.appendChild(mediaElement);
+		let mediaContainer = document.createElement("div");
+		if(rdmPost.secure_media || rdmPost.secure_media_embed.content || rdmPost.url.includes(".gifv")){
+			mediaContainer.innerHTML = this.buildVideo(rdmPost);
+		}
+		else{
+			mediaContainer.innerHTML = this.buildImg(rdmPost)
+		}
+		wrapper.appendChild(mediaContainer);
 		return wrapper;
 	},
 	buildVideo(video){
-		let videoElement = document.createElement("video");
-		videoElement.classList.add("aww-video");
-		video.setAttribute("autoplay", "");
-		video.setAttribute("loop", "");
-		let source = document.createElement("source");
-		source.src = video.secure_media.reddit_video.fallback_url;
-		videoElement.appendChild("source");
-		return videoElement;
+		if(!video.secure_media_embed.content){
+			let src = video.secure_media ? video.secure_media.reddit_video.fallback_url : video.url.replace("gifv", "mp4");
+			return `<video class="aww-video" muted autoplay loop src=${src}></video>`;
+		}
+		return `<img class="aww-img" src="${video.secure_media.oembed.thumbnail_url}">`
 	},
 	buildImg(img){
-		let imgElement = document.createElement("img");
-		imgElement.classList.add("aww-img");
-		imgElement.src = img.preview.images[0].source.url;
-		return img;
-	}
-
+		return `<img class="aww-img" src=${img.url}>`;
+	},
 });
